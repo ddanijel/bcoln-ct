a2e659374ec537e08e264990c930b066879b1287
claudemiller <claude.muller@protonmail.com>
2019-05-12 16:44:43 +0200

    Extended smart contract.

--- src/ethereum/contracts/Lottery.sol
+++ src/ethereum/contracts/Lottery.sol
@@ -2,9 +2,10 @@ pragma solidity >=0.4.22 <0.6.0;
 
 contract LotteryFactory {
     Lottery[] public deployedLotteries;
-
-    function createLottery(uint priceToPlay) public {
-        Lottery newLottery = new Lottery(priceToPlay, msg.sender);
+    
+    function createLottery(uint priceToPlay, uint minGuess, uint maxGuess) public {
+        require(maxGuess > minGuess);
+        Lottery newLottery = new Lottery(priceToPlay, minGuess, maxGuess, address(this), msg.sender);
         deployedLotteries.push(newLottery);
     }
 
@@ -17,24 +18,32 @@ contract LotteryFactory {
 contract Lottery {
 
     address public owner;
+    address payable public factory;
     uint public ticketPrice;
     uint playersCount = 0;
+    uint minGuess;
+    uint maxGuess;
+    bool played = false;
 
     struct Player {
-        address playerAddress;
+        address payable playerAddress;
         uint guess;
     }
 
     Player[] public players;
+    Player[] public winners;
 
-    constructor(uint priceToPlay, address manager) public {
-        owner = manager;
+    constructor(uint priceToPlay, uint _minGuess, uint _maxGuess, address _factory, address _owner) public {
+        owner = _owner;
+        factory = address(uint160(_factory));
         ticketPrice = priceToPlay;
+        minGuess = _minGuess;
+        maxGuess = _maxGuess;
     }
 
     function play(uint guess) public payable {
-        require(msg.value > ticketPrice);
-        require(guess > 0 && guess < 10);
+        require(msg.value == ticketPrice);
+        require(guess >= minGuess && guess <= maxGuess);
 
         Player memory newPlayer = Player({
             playerAddress : msg.sender,
@@ -43,13 +52,38 @@ contract Lottery {
         playersCount++;
         players.push(newPlayer);
     }
+    
+    function pickWinner() public {
+        require(msg.sender == owner);
+        // TODO get random number
+        uint rndNr = 4;
+        
+        for (uint i=0; i < players.length; i++) {
+            if (players[i].guess == rndNr) {
+                winners.push(players[i]);
+            }
+        }
+        
+        factory.transfer(address(this).balance);
+        if (winners.length >= 1) {
+            uint prizePerWinner = address(this).balance/winners.length; // prize  in Wei
+            for (uint i=0; i < winners.length; i++) {
+                winners[i].playerAddress.transfer(prizePerWinner);
+            }
+        } else {
+            factory.transfer(address(this).balance);
+        }
+        played = true;
+    }
 
     function describeLottery() public view returns (
-        uint, uint, address
+        uint, uint, uint, uint, address
     ) {
         return (
         playersCount,
         ticketPrice,
+        minGuess,
+        maxGuess,
         owner
         );
     }
