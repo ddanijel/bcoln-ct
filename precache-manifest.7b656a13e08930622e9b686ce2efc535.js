9c46c3122df1a6b14eaea40eed34fcda8a19f6a0
danijel <danijel.fon@gmail.com>
2019-05-17 20:28:47 +0200

    Updates

---
+++ precache-manifest.7b656a13e08930622e9b686ce2efc535.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecc2f658fc5e9f02d2d6a609e522ea8",
+    "url": "/bcoln-ct/index.html"
+  },
+  {
+    "revision": "1cadbbea0871603b4fee",
+    "url": "/bcoln-ct/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "979ed0c50b9b8ec0a0ed",
+    "url": "/bcoln-ct/static/js/2.b233e776.chunk.js"
+  },
+  {
+    "revision": "1cadbbea0871603b4fee",
+    "url": "/bcoln-ct/static/js/main.5085ef97.chunk.js"
+  },
+  {
+    "revision": "61996e39c52d4e69e128",
+    "url": "/bcoln-ct/static/js/runtime~main.21583021.js"
+  }
+]);
